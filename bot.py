5416061adaea4d4b263d36792d7411db72423a0b
Happylama25 <git@happyllama25.net>
2022-12-18 14:45:50 -0500

    yeah

---
+++ bot.py
@@ -0,0 +1,46 @@
+import disnake
+from rcon.source import Client
+from disnake.ext import commands
+from enum import Enum
+
+
+bot = commands.Bot(command_prefix=disnake.ext.commands.when_mentioned,test_guilds=[<your-guild-ID-here>])
+
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}.")
+
+
+@bot.slash_command(description="Blep (also used as ping)")
+async def ping(inter: disnake.ApplicationCommandInteraction):
+    await inter.response.send_message(f'{round(bot.latency * 1000)}ms')
+
+
+class Users(str, Enum):
+    Player1 = 'SteamID1'
+    Player2 = 'SteamID2'
+    Player3 = 'SteamID3'
+    
+@bot.slash_command(description="Lists connected players")
+# @commands.has_role(733408652077170785) #Optional, you can specify an admin role ID here to only allow certain roles to perform the command
+async def players(inter: disnake.ApplicationCommandInteraction, user: Users):
+    with Client('<Server IP>', <RCON Port>, passwd='<AdminPassword>') as client:
+        response = client.run('listplayers')
+        await inter.response.send_message(f'{response}')
+
+@bot.slash_command(description="Kicks a player from the ARK Server")
+# @commands.has_role(733408652077170785) #Optional, you can specify an admin role ID here to only allow certain roles to perform the command
+async def kickplayer(inter: disnake.ApplicationCommandInteraction, user: Users):
+    with Client('<Server IP>', <RCON Port>, passwd='<AdminPassword>') as client:
+        response = client.run(f'kickplayer {user}')
+        await inter.response.send_message(f'Kicked player')
+
+@kickplayer.error
+async def kickplayer_error(ctx, error):
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send("You do not have the required role and cannot use this command.")
+
+
+bot.run("<Bot-Token>")
